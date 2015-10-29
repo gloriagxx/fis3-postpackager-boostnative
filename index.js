409035659bac286454d0ecc9e15cf224c5e85238
jiexuangao <jiexuangao@gmail.com>
2015-10-29 11:33:48 +0800

    update plugins

--- index.js
+++ index.js
@@ -2,19 +2,17 @@
 
 module.exports = function(ret, conf, settings, opt) {
     var content = 'window.boost && boost.addStyle && boost.addStyle("';
+    var boostFile = ret.src[settings.addTo];
 
     for (var key in ret.src) {
         var item = ret.src[key];
         if (key.indexOf('.css') >= 0) {
             content += item._content.replace(/[\n]/ig, '');
         }
+
     }
 
     content += '")';
-    var path = fis.project.getProjectPath() + '/static/js/boostNative.js';
 
-    var file = fis.file(path);
-    file.setContent(content);
-
-    ret.pkg[file.subpath] = file;
+    boostFile.setContent(boostFile._content + content);
 }
